7308388c7b1be2d2bb874873556a1aede31c31ff
jlec0001 <jlec0001@student.monash.edu>
2021-12-09 21:34:06 +1100

    Added product routes

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ app.use(express.json());
 // Importing routes
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ mongoose
 // Using routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running");
