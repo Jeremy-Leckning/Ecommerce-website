f3b4921e921a8913da7fd64e14aa56ae2f65c302
jlec0001 <jlec0001@student.monash.edu>
2021-12-11 16:30:03 +1100

    Refactored frontend files to jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import ProductList from "./pages/ProductList/ProductList";
+import Register from "./pages/Register/Register";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
+import SingleProduct from "./pages/SingleProduct/SingleProduct";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Home />
+      {/* <ProductList /> */}
+      {/* <SingleProduct /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      {/* <ShoppingCart /> */}
+    </div>
+  );
 };
 
 export default App;
